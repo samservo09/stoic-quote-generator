319f714d0913b5c4c5c13e012de1edaeaf19ee6c
Vivien <samanthaservo09@gmail.com>
2024-06-02 10:29:56 +0800

    finished getting stoic quotes from API

--- stoic.py
+++ stoic.py
@@ -7,4 +7,9 @@ response_API = requests.get('https://stoic.tekloon.net/stoic-quote')
 #print(response_API.status_code) #if 200, it is success
 
 # get data from API
+stoic_data = response_API.text 
 
+# parse data into JSON format
+parse_json = json.loads(stoic_data)
+
+# extract data
