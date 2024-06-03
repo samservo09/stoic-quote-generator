6b8cc01bf4c5d89951d6ff5261c572f2d1c66a3b
Vivien <samanthaservo09@gmail.com>
2024-06-03 23:34:43 +0800

    added the base code

--- stoic.py
+++ stoic.py
@@ -13,3 +13,33 @@ stoic_data = response_API.text
 
 # parse data into JSON format
 parse_json = json.loads(stoic_data)
+
+# convert the body dictionary into a string
+body_content = f"{parse_json['quote']} - {parse_json['author']}"
+
+# function to send the email
+def email_message(subject, body, to):
+    msg = EmailMessage() # create message from the library
+    msg.set_content(body) # call method 
+    msg['subject'] = subject
+    msg['to'] = to 
+    
+    # create variable for gmail user
+    user = "stoic.msg.4u@gmail.com"
+    msg['from'] = user
+    password = "syqcdtlejotfsldn"
+    
+    # set server parameters
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+    server.quit()
+    
+print("Do you want to receive emails about stoicism?")
+
+gmail_acc = input("Enter your gmail account: ")
+
+
+if __name__ == '__main__':
+    email_message("Hello! Here is your daily stoic message!", body_content, gmail_acc)
