a2c27df866589b326307eda93312f38be82eef49
Vivien <samanthaservo09@gmail.com>
2024-06-12 22:49:18 +0800

    fixed the python interpreter in app py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 import subprocess
+import sys
 
 app = Flask(__name__) # creates a class for our app
 app.secret_key = 'syqcdtlejotfsldn'
@@ -16,8 +17,10 @@ def process():
         send_time = request.form['send_time']
         str_duration = request.form['str_duration']
         
+        python_interpreter = sys.executable
+        
         # call the stoic.py script to process sending stoic emails
-        subprocess.Popen(['python3', 'stoic.py', gmail_acc, send_time, str_duration])
+        subprocess.Popen([python_interpreter, 'stoic.py', gmail_acc, send_time, str_duration])
         
         # feedback to the user
         flash("You are now subscribed to receive daily stoic emails!", "Success!")
